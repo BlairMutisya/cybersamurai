00c5886a8e20ecc82e9ed85252182986fd219dcb
BlairMutisya <tonnyblair09@gmail.com>
2024-09-30 00:04:24 +0300

    Update on the images

--- script.js
+++ script.js
@@ -175,11 +175,20 @@ tabList.addEventListener("click", (e) => {
   );
 });
 
-const swiper = new Swiper(".swiper", {
-  slidesPerView: "auto",
+var swiper = new Swiper(".swiper", {
+  slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
+
 
 ScrollReveal().reveal(".blog__card", {
   ...scrollRevealOption,
